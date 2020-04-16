f883582f6f5f87a73deee36022804a7fafbfc0b4
CesarWilliam <will.roc@hotmail.com>
2020-04-16 18:27:48 -0300

    tipos

--- curso-typescript/tipos/tipos.js
+++ curso-typescript/tipos/tipos.js
@@ -1,14 +1,14 @@
 "use strict";
-// === string
+// ======= string ======= //
 var nome = 'João';
 console.log(nome);
-// === number
+// ======= number ======= //
 var idade = 27;
 console.log(idade);
-// === boolean
+// ======= boolean ======= //
 var possuiHobbies = false;
 console.log(possuiHobbies);
-// === tipos explicitos
+// ======= tipos explicitos ======= //
 var minhaIdade;
 minhaIdade = 27;
 console.log(typeof minhaIdade);
@@ -18,15 +18,15 @@ console.log(typeof minhaIdade);
    quando o tipo é implicito, a não inicialização da variável já com um valor declarado
    determina o tipo da mesma conforme a declaração de valores na propria variável.
 */
-// === array
+// ======= array ======= //
 var hobbies = ['Cozinhar', 'Praticar Esportes'];
 console.log(hobbies);
 console.log(typeof hobbies);
 hobbies = [100, 200];
-// === tuplas
+// ======= tuplas ======= //
 var endereco = ['Av Principal', 99];
 console.log(endereco);
-// === enums
+// ======= enums ======= //
 // estrutura que define valores pré-definidos
 var Cor;
 (function (Cor) {
@@ -36,10 +36,10 @@ var Cor;
 })(Cor || (Cor = {}));
 var minhaCor = Cor.Verde;
 console.log(minhaCor);
-// === any
+// ======= any ======= //
 var carro = 'BMW';
 console.log(carro);
-// === funções
+// ======= funções ======= //
 function retornaMeuNome() {
     return nome;
 }
@@ -51,8 +51,49 @@ function multiplicar(numA, numB) {
     return numA * numB;
 }
 console.log(multiplicar(4, 8));
-// === tipo função
+// ======= tipo função ======= //
 // nessa demonstração, foi tipado em uma variável uma função
 var calculo;
 calculo = multiplicar;
 console.log(calculo(5, 6));
+// ======= objetos ======= //
+// conforme toda a regra de typescript, uma vez criado um objeto, ja inicializando a variável com propriedades pré definidas
+// essa váriavel fica com a tipagem já definida
+var usuario = {
+    nome: 'João',
+    idade: 27
+};
+// ======= desafio ======= //
+/*
+    Criar um objeto funcionário com:
+    - Array de strings com os nomes dos supervisores
+    - Função de bater ponto que recebe a hora (número) e retorna uma string
+        -> Ponto normal (<= 8)
+        -> Fora do horário (> 8)
+*/
+var funcionarios = {
+    supervisores: ['Jorge', 'Maria'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
+    }
+};
+var funcionarios2 = {
+    supervisores: ['Jorge', 'Maria'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
+    }
+};
+/* O exemplo acima é o mesmo do anterior, mas com o uso do alias, para deixar genérico o tipo */
+// ======= Union Types ======= // 
+// utiliza o '|' para 'ou', podendo ser dois tipos na mesma variável
+var nota = 10;
--- curso-typescript/tipos/tipos.ts
+++ curso-typescript/tipos/tipos.ts
@@ -1,16 +1,16 @@
-// === string
+// ======= string ======= //
 let nome: string = 'João'
 console.log(nome)
 
-// === number
+// ======= number ======= //
 let idade: number = 27
 console.log(idade)
 
-// === boolean
+// ======= boolean ======= //
 let possuiHobbies: boolean = false
 console.log(possuiHobbies)
 
-// === tipos explicitos
+// ======= tipos explicitos ======= //
 let minhaIdade
 minhaIdade = 27
 console.log(typeof minhaIdade)
@@ -22,18 +22,18 @@ console.log(typeof minhaIdade)
    determina o tipo da mesma conforme a declaração de valores na propria variável.
 */
 
-// === array
+// ======= array ======= //
 let hobbies: any[] = ['Cozinhar', 'Praticar Esportes']
 console.log(hobbies)
 console.log(typeof hobbies)
 
 hobbies = [100, 200]
 
-// === tuplas
+// ======= tuplas ======= //
 let endereco: [string, number] = ['Av Principal', 99]
 console.log(endereco);
 
-// === enums
+// ======= enums ======= //
 // estrutura que define valores pré-definidos
 enum Cor {
     Cinza, // = 0 
@@ -44,11 +44,11 @@ enum Cor {
 let minhaCor: Cor = Cor.Verde;
 console.log(minhaCor);
 
-// === any
+// ======= any ======= //
 let carro: any = 'BMW'
 console.log(carro)
 
-// === funções
+// ======= funções ======= //
 function retornaMeuNome(): string { // diz explicitamente qual o tipo do retorno da função
     return nome
 }
@@ -65,8 +65,60 @@ function multiplicar(numA: number, numB: number): number {
 
 console.log(multiplicar(4, 8))
 
-// === tipo função
+// ======= tipo função ======= //
 // nessa demonstração, foi tipado em uma variável uma função
 let calculo: (numeroA: number, numeroB: number) => number
 calculo = multiplicar
 console.log(calculo(5, 6))
+
+// ======= objetos ======= //
+// conforme toda a regra de typescript, uma vez criado um objeto, ja inicializando a variável com propriedades pré definidas
+// essa váriavel fica com a tipagem já definida
+let usuario: { nome: string, idade: number } = {
+    nome: 'João',
+    idade: 27
+}
+
+// ======= desafio ======= //
+/*
+    Criar um objeto funcionário com:
+    - Array de strings com os nomes dos supervisores
+    - Função de bater ponto que recebe a hora (número) e retorna uma string
+        -> Ponto normal (<= 8)
+        -> Fora do horário (> 8)
+*/
+
+let funcionarios: { supervisores: string[], baterPonto: (horas: number) => string } = {
+    supervisores: ['Jorge', 'Maria'],
+    baterPonto(horario: number): string {
+        if (horario <= 8) {
+            return 'Ponto normal'
+        } else {
+            return 'Fora do horário'
+        }
+    }
+}
+
+// ======= Alias ======= //
+// um 'tipo' pré definido
+type Funcionario = {
+    supervisores: string[], 
+    baterPonto: (horas: number) => string
+}
+
+let funcionarios2: Funcionario = {
+    supervisores: ['Jorge', 'Maria'],
+    baterPonto(horario: number): string {
+        if (horario <= 8) {
+            return 'Ponto normal'
+        } else {
+            return 'Fora do horário'
+        }
+    }
+}
+
+/* O exemplo acima é o mesmo do anterior, mas com o uso do alias, para deixar genérico o tipo */
+
+// ======= Union Types ======= // 
+// utiliza o '|' para 'ou', podendo ser dois tipos na mesma variável
+let nota: number | string = 10
