1989a9b8f79fbe0b2a018344ab5f3d2133e3b5f2
CesarWilliam <will.roc@hotmail.com>
2020-11-24 21:56:06 -0300

    generics

--- curso-typescript/genericos/genericos.js
+++ curso-typescript/genericos/genericos.js
@@ -23,4 +23,79 @@ imprimir([
     { nome: 'Teste Aluno 2', idade: 20 },
     { nome: 'Teste Aluno 3', idade: 30 },
 ]);
+// ======= Usando generics com Classe ======= //
+// <T, R> => é possível passar N "parâmetros" genéricos dentro da sua classe
+// o valor (tipo) declarado em cada parâmetro define o tipo da propriedade
+// Ex:
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+}
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new SomaBinaria(3, 4).executar());
+// Outro ex:
+class DiferencaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(7, 2, 2020);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
+// Desafio
+// Atributo: fila (array)
+// Métodos: entrar, proximo, imprimir
+class Fila {
+    constructor(...args) {
+        this.fila = args;
+    }
+    entrar(elemento) {
+        this.fila.push(elemento);
+    }
+    proximo() {
+        if (this.fila.length >= 0 && this.fila[0]) {
+            const primeiro = this.fila[0];
+            this.fila.splice(0, 1);
+            return primeiro;
+        }
+        else {
+            return null;
+        }
+    }
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+const fila = new Fila('Will', 'Elvis', 'Lucas');
+fila.imprimir();
+// ======= Restrições (Constraints) ======= //
+// com o "extends" é criado uma restrição para o generics
+// assim não podendo passar qualquer tipo como parâmetro na classe
+// Ex:
+class Fila2 {
+    constructor(...args) {
+        this.fila = args;
+    }
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+// Desafio Mapa
+// Array de objetos (Chave/Valor) -> itens
+// Métodos: obter(Chave), colocar({ C, V })
+// limpar(), imprimir()
 //# sourceMappingURL=genericos.js.map
--- curso-typescript/genericos/genericos.ts
+++ curso-typescript/genericos/genericos.ts
@@ -30,3 +30,111 @@ imprimir<Aluno>([
     { nome: 'Teste Aluno 2', idade: 20 },
     { nome: 'Teste Aluno 3', idade: 30 },    
 ]);
+
+
+// ======= Usando generics com Classe ======= //
+// <T, R> => é possível passar N "parâmetros" genéricos dentro da sua classe
+// o valor (tipo) declarado em cada parâmetro define o tipo da propriedade
+// Ex:
+abstract class OperacaoBinaria<T, R> {  
+    constructor(
+        public operando1: T,
+        public operando2: T
+    ){}
+
+    abstract executar(): R
+}
+
+class SomaBinaria extends OperacaoBinaria<number, number> {
+    executar(): number {
+        return this.operando1 + this.operando2;
+    }
+}
+
+console.log(new SomaBinaria(3, 4).executar());
+
+// Outro ex:
+class DiferencaEntreDatas extends OperacaoBinaria<Data, string> {
+    getTime(data: Data): number {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+
+    executar(): string {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca/dia)} dia(s)`;
+    }
+}
+
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(7, 2, 2020);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
+
+// Desafio
+// Atributo: fila (array)
+// Métodos: entrar, proximo, imprimir
+class Fila<T> {
+    private fila: Array<T>;
+
+    constructor(...args: T[]) {
+        this.fila = args;
+    }
+
+    entrar(elemento: T) {
+        this.fila.push(elemento);
+    } 
+
+    proximo(): T | null {
+        if (this.fila.length >= 0 && this.fila[0]) {
+            const primeiro = this.fila[0];
+            this.fila.splice(0, 1);
+            return primeiro;
+        }
+        else {
+            return null;
+        }
+    }
+
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+
+const fila = new Fila<string>('Will', 'Elvis', 'Lucas');
+fila.imprimir();
+
+
+// ======= Restrições (Constraints) ======= //
+// com o "extends" é criado uma restrição para o generics
+// assim não podendo passar qualquer tipo como parâmetro na classe
+// Ex:
+class Fila2<T extends number | string> {
+    private fila: Array<T>;
+
+    constructor(...args: T[]) {
+        this.fila = args;
+    }
+
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+
+// Desafio Mapa
+// Array de Objetos (Chave/Valor) -> itens
+// Métodos: obter(Chave), colocar({ C, V })
+// limpar(), imprimir()
+ 
+// const mapa = new Mapa<number, string>()
+// mapa.colocar({ chave: 1, valor: 'Pedro' })
+// mapa.colocar({ chave: 2, valor: 'Rebeca' })
+// mapa.colocar({ chave: 3, valor: 'Maria' })
+// mapa.colocar({ chave: 1, valor: 'Gustavo' })
+ 
+// console.log(mapa.obter(2))
+// mapa.imprimir()
+// mapa.limpar()
+// mapa.imprimir()
